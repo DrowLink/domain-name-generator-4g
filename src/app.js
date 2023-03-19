ab0e34de37e60edc57ada1165f1e5efdb0de43a9
Paulo Riveiro <pauloriveiro01@gmail.com>
2023-03-19 20:49:20 +0000

    Not finished yet 🚧

--- src/app.js
+++ src/app.js
@@ -7,5 +7,62 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function random() {
+    return Math.floor(Math.random() * 2);
+  }
+
+  function domainNameGenerator() {
+    let pronounAdjRandomized = [];
+    let fullyRandomizedVariable = [];
+    let pronounAdjRandomizedFiltered = [];
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+
+    console.log(random());
+
+    function pronounAdjPermuts() {
+      for (let i = 0; i < 4; i++) {
+        pronounAdjRandomized.push(pronoun[random()] + adj[random()]);
+
+        // pronounAdjRandomized.push(pronoun[1] + adj[0]);
+        // pronounAdjRandomized.push(pronoun[1] + adj[1]);
+        // pronounAdjRandomized.push(pronoun[0] + adj[0]);
+      }
+
+      pronounAdjRandomizedFiltered = pronounAdjRandomized.filter(() => {
+        for (let i = 0; i < pronounAdjRandomized.length; i++) {
+          pronounAdjRandomized[i] != pronounAdjRandomized;
+        }
+      });
+
+      return pronounAdjRandomizedFiltered;
+    }
+
+    pronounAdjPermuts();
+
+    function fullyRandomized() {
+      for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+          let content = pronounAdjRandomizedFiltered[i] + noun[random];
+          fullyRandomizedVariable.push(content);
+        }
+      }
+      return fullyRandomizedVariable; //Hasta aca todo bien
+    }
+
+    fullyRandomized();
+
+    // function deleteRepeateds(fullyRandomizedVariable) {
+    //   for () {}
+    // }
+
+    console.log(pronounAdjPermuts(pronounAdjRandomizedFiltered));
+
+    console.log(fullyRandomizedVariable);
+
+    console.log("Hello Rigo from the console!");
+  }
+
+  domainNameGenerator();
 };
